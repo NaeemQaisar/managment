29a3a4b44188c6ad0014a15b4b67192cc726f9a5
NaeemQaisar <mr.naeem2000@gmail.com>
2015-12-26 16:12:05 -0500

    Changing in Students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -51,7 +51,7 @@ class StudentsController < ApplicationController
     
     respond_to do |format|
       if @student.save
-        format.html { redirect_to classrooms_path, notice: 'Student was successfully created.' }
+        format.html { redirect_to @classroom, notice: 'Student was successfully created.' }
         format.json { render json: @student, status: :created, location: @student }
       else
         format.html { render action: "new" }
