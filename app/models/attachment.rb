bada4dc7a111add9feb752f299ecf2b6604f50aa
NaeemQaisar <mr.naeem2000@gmail.com>
2015-12-26 15:48:35 -0500

    Added style in applictaion.css

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
 
   attr_accessible :image
-  has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
